6a4a9d85c770ad71994482fda62fd4df42c40359
Denis Krienbühl <denis.krienbuehl@cloudscale.ch>
2025-02-27 17:02:58 +0100

    Scope retries more clearly
    - We do not want to retry on HTTP 400, unless it is a DELETE.
    - We want to log the retries with the corresponding request.

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from constants import LOCKS_PATH
 from constants import PROCESS_ID
 from constants import RUNNER_ID
 from errors import Timeout
-from events import record, trigger
+from events import trigger
 from filelock import FileLock
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -26,6 +26,33 @@ class CloudscaleHTTPAdapter(HTTPAdapter):
     def send(self, request, *args, **kwargs):
         with self.lock:
             return super().send(request, *args, **kwargs)
+
+
+class RetryStrategy(Retry):
+    """ We retry certain requests using urllib3's Retry class, but we
+    need to be quite explicit about when, in a way that is more complicated
+    than Retry's config approach.
+
+    """
+
+    def is_retry(self, method, status_code, *args, **kwargs) -> bool:
+        """ This method has to decide if a retry should be attempted, it does
+        not need to know how many retries have been done already.
+
+        """
+
+        # DELETE may fail on resources during cleanup, as they may be
+        # still be in the process of being created.
+        if status_code == 400 and method == 'DELETE':
+            return True
+
+        # Maintenances are always retried.
+        if status_code == 503:
+            return True
+
+        # Fallback to the default retry handling otherwise (which supports
+        # the Retry-After header).
+        return super().is_retry(method, status_code, *args, **kwargs)
 
 
 class API(requests.Session):
@@ -54,16 +81,9 @@ class API(requests.Session):
         self.scope = scope
         self.read_only = read_only
 
-        # The API may be in maintenance (HTTP 503) or
-        # DELETE may fail on resources when they are being created,
-        # so we retry those a number of times.
-        #
         # 8 Retries @ 2.5 backoff_factor = 10.6 minutes
-        retry_strategy = Retry(
+        retry_strategy = RetryStrategy(
             total=8,
-            status_forcelist=[400, 503],
-            allowed_methods=["HEAD", "GET", "POST", "DELETE"],
-            raise_on_status=False,  # Prevent Exception after every request
             backoff_factor=2.5
         )
 
@@ -112,14 +132,6 @@ class API(requests.Session):
 
     def on_response(self, response, *args, **kwargs):
         trigger('request.after', request=response.request, response=response)
-
-        # Record the history if Retry was used
-        if response.raw.retries.history:
-            record(
-                'request.retry',
-                retries=len(response.raw.retries.history),
-                history=response.raw.retries.history
-            )
 
         try:
             response.raise_for_status()
